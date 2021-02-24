2920523dd4c6f2e21c8128738506956f97e1718b
Jason Hales <jason.hales@g.austincc.edu>
2021-02-23 23:06:19 -0600

    Javascript: Uploading index.html, script.js, and style.css for Project 1

---
+++ Javascript/Project01/script.js
@@ -0,0 +1,9 @@
+
+
+let myName = prompt("Please enter your name?");
+let mySnack= prompt("What is your favorite snack?");
+let myDrink = prompt("What is your favorite drink?");
+
+
+let result = `Hi there ${myName}, it seems that you enjoy eating ${mySnack} and drinking ${myDrink}!`;
+alert(result);
