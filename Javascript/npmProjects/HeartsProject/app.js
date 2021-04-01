28960e1b6f772aa81d23316549b51b3f8f4e7976
Jason Hales <jason.hales@g.austincc.edu>
2021-04-01 14:26:08 -0500

    NODE NPM:  Task 1 and 2 HeartsProject

---
+++ Javascript/npmProjects/HeartsProject/app.js
@@ -0,0 +1,12 @@
+var asciiHeart = require("ascii-heart");
+ 
+// Default behavior
+console.log(asciiHeart());
+
+
+// Display a small heart (width x height) <3
+console.log(asciiHeart(10, 10));
+
+
+
+console.log(asciiHeart(40, 40, {fill: "?"} ) );
