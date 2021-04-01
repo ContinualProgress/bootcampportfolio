e72cf188f64194e4efa431994c32384cdcf4e2a4
Jason Hales <jason.hales@g.austincc.edu>
2021-04-01 16:20:14 -0500

    NODE NPM:  Task 4 - Casual

---
+++ Javascript/npmProjects/casual/app.js
@@ -0,0 +1,13 @@
+var casual = require('casual');
+console.log("Type (casual):  " + typeof casual);
+//console.log(casual);
+
+
+console.log("Hello there " + casual.name + " !");
+console.log("How was your trip to " + casual.country + " ?");
+console.log("Did you get to visit " + casual.city + " ?");
+console.log("I sure do hope you had a wonderful time.");
+console.log("Is your phone number still " + casual.phone + "?");
+console.log("I will try to give you a call sometime. By the way, I want to send you a fresh loaf of bread at your address of:  " + casual.address + "." );
+console.log("Well, I will see you soon. I will be visiting sometime before " + casual.month_name + ".");
+console.log("Until then, farewell!");
