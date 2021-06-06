f2be532bef91ff55a4299f26209d4ed4663e421c
Jason Hales <jason.hales@g.austincc.edu>
2021-06-06 16:23:56 -0500

    Advanced Javascript:  Updates to Project 1

--- Javascript/advanced/Project01/grader.js
+++ Javascript/advanced/Project01/grader.js
@@ -25,12 +25,23 @@ function grade(a, b){
 
 
 let individualQuiz = ["A", "B", "B", "D", "C"];
+let individualQuiz2  = ["C", "D", "B", "D", "C"];
 let answerKey = ["C", "D", "B", "D", "C"];
 
 
 
+//When student get some wrong answers...
 score = grade(individualQuiz, answerKey);
 
 console.log( "Quiz:  " + individualQuiz);
+console.log( "Answer Key:  " + answerKey);
+console.log( "Score:  " + score);
+
+
+//When student gets all correct answers...
+score = grade(individualQuiz2, answerKey);
+
+console.log("\n\n");
+console.log( "Second Quiz:  " + individualQuiz2);
 console.log( "Answer Key:  " + answerKey);
 console.log( "Score:  " + score);
