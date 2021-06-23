25272c0f6582942bc1449170b4cfb71fb795415e
Jason Hales <jason.hales@g.austincc.edu>
2021-06-23 18:49:22 -0500

    React:  Project 1 Part 4

--- react/john-arbuckle/src/Arbuckle01.js
+++ react/john-arbuckle/src/Arbuckle01.js
@@ -5,11 +5,17 @@ import arbuckle01 from './arbuckle01.jpg';
 class Arbuckle01 extends Component {
 
   render() {
+
+    const imageBorder = {
+
+      border: '2px solid blue',
+
+    }
   
     return(
       <div>
 	<h1>About Jon Arbuckle</h1>
-	<img src={arbuckle01} alt="arbuckle01" />
+	<img src={arbuckle01} alt="arbuckle01" style={imageBorder}/>
 
 	<p>
 	  Hi there everyone.  My name is <u>Jon Arbuckle</u>.  I am 30 years old and I am a cartoonist that lives in Munice, Indiana.  I am the proud owner of a cat named <strong>Garfield</strong> and a dog named <strong>Odie</strong>.  Garfield is fat, lazy and loves to eat, while Odie is a dog without a brain.  Although my pets drive me crazy, I can't imagine my life without them.
