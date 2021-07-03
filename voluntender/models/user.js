52b8c0e8cd52126ffdff9678c6848385a6acaa02
Jason Hales <jason.hales@g.austincc.edu>
2021-07-02 19:59:11 -0500

    Updated current voluntender

---
+++ voluntender/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+let UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    pic: String,
+    gender: String,
+    ageRange: String,
+    bio: String,
+    interests: Array,
+    lookingFor: Array
+});
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', UserSchema);
